b3da78859fd00b78abdaa1101f4c7083a5d75407
Maye85 <mayegabrielaboffil@gmail.com>
2024-09-14 11:56:57 -0500

    mejorando el chatindividual

--- src/views/chatIndividual.js
+++ src/views/chatIndividual.js
@@ -19,6 +19,8 @@ async function chatIndividual({ id }) {
     const containerDiv = document.createElement('div');
     containerDiv.classList.add('contenedor-imagen-chat'); // Clase para estilos CSS
 
+
+    
     // Crear la vista del personaje con imagen y detalles
     const viewEl = document.createElement('div');
     viewEl.classList.add('viewEl-content'); // Nueva clase para aplicar estilos específicos si es necesario
@@ -33,6 +35,16 @@ async function chatIndividual({ id }) {
     chatButton.id = 'chat-button';
     chatButton.innerText = 'Chatear';
 
+    // Crear el botón para regresar a Home
+    const botonHome = document.createElement('button');
+    botonHome.id = 'botonHome';
+    botonHome.innerText = 'Regresar a Home';
+
+    // Agregar el evento de click al botón para regresar a Home
+    botonHome.addEventListener('click', () => {
+      window.location.href = '/'; // Asegúrate de ajustar la ruta a tu home si es diferente
+    });
+
     // Crear el componente del chat y agregarlo al contenedor
     const chatComponent = renderChatComponent(character);
     chatComponent.style.display = 'none'; // Ocultar el chat inicialmente
@@ -45,9 +57,11 @@ async function chatIndividual({ id }) {
     chatContainerDiv.appendChild(chatButton);
     chatContainerDiv.appendChild(chatComponent);
 
-    // Agregar la vista del personaje y el contenedor del chat al contenedor principal
+    // Agregar la vista del personaje, el contenedor del chat y el botón Home al contenedor principal
     containerDiv.appendChild(viewEl); 
     containerDiv.appendChild(chatContainerDiv);
+
+    containerDiv.appendChild(botonHome); // Aquí agregamos el botón Home dentro del contenedor
 
     // Agregar todo al divHome
     divHome.appendChild(containerDiv);
